093ce68feb57579dcff624f30d7acfa8a92df2f2
Idan Beker <idanbek@gmail.com>
2017-01-06 15:22:06 +0200

    added template, shop working

--- shop/models.py
+++ shop/models.py
@@ -1,12 +1,22 @@
 from django.db import models
-
+from django.core.urlresolvers import reverse
 
 class User(models.Model):
     name = models.CharField(max_length=200)
     cart = models.CharField(max_length=200)
+    cart_size = models.IntegerField()
+    page_index = models.IntegerField()
+
+    def __str__(self):
+        return self.name
 
 
 class Item(models.Model):
     name = models.CharField(max_length=250)
     desc = models.CharField(max_length=400)
+    pic_url = models.CharField(max_length=300)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
+
--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render
-from .models import User , Item
+from django.views.generic.edit import CreateView, UpdateView
+from django.views import generic
+from .models import User, Item
 
 
 def index(request):
     user = User.objects.get(id='1')
-    context = {'user' : user }
-    return render(request,'shop/shop.html',context)
+    items = Item.objects.all()
+    context = {'user': user, 'items': items}
+    if request.POST.get('mybtn'):
+        user.cart_size += int(request.POST.get('quantity'))
+        user.save()
+    return render(request, 'shop/shop.html', context)
+
 
 def cart(request):
     user = User.objects.get(id='1')
-    context = {'user' : 'test'}
-    return render(request, 'shop/cart.html',context)
+    context = {'user': user}
+    return render(request, 'shop/cart.html', context)
+
+
+
+
 
