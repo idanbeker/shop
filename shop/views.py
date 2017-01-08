eaf9d78867422a3497a69570d9a47d2b5c919044
Idan Beker <idanbek@gmail.com>
2017-01-08 15:37:55 +0200

    added multiple pages to shop

--- shop/views.py
+++ shop/views.py
@@ -3,11 +3,20 @@ from .models import User, Item
 import logging
 
 
-
 def index(request):
     user = User.objects.get(id='1')
-    items = Item.objects.all()
-    context = {'user': user, 'items': items}
+
+    # Next page case - change item filter
+    if request.POST.get('gotopage'):
+        user.page_index = int(request.POST.get('topage')) - 1
+        user.save()
+    items = selectItems(user)
+
+    context = {'user': user,
+               'items': items,
+               'num_pages': (Item.objects.all().__len__() % 6) + 1
+               }
+    # Add item to cart case
     if request.POST.get('additem'):
         addToCart(request, context)
 
@@ -27,13 +36,12 @@ def cart(request):
         quantity = cart_list.count(x)
         total_item = quantity * item.price
         total_cart += total_item
-        cart_item = {'item' : item ,
-                'quantity' : quantity ,
-                'total' : total_item
-                }
+        cart_item = {'item': item,
+                     'quantity': quantity,
+                     'total': total_item
+                     }
         cart.append(cart_item)
-    print(type(cart))
-    context = {'user': user, 'cart' : cart ,'items' :items , 'total': total_cart}
+    context = {'user': user, 'cart': cart, 'items': items, 'total': total_cart}
     return render(request, 'shop/cart.html', context)
 
 
@@ -47,3 +55,10 @@ def addToCart(request, context):
     else:
         user.cart += (' ' + request.POST.get('productid')) * int(request.POST.get('quantity'))
     user.save()
+
+
+def selectItems(user):
+    items = Item.objects.all()
+    current_page = user.page_index
+
+    return items.filter(id__gt=current_page * 6).filter(id__lt=1 + (current_page + 1) * 6)
